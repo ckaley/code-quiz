99c24719e2472b6744cdaa47ce0da7a0d9aba9ae
Craig Kaley <craig@caucusroom.com>
2020-04-18 22:26:59 -0600

    Saving just to be safe.  Still work in progress.

--- script.js
+++ script.js
@@ -1,147 +1,191 @@
 $(document).ready(function () {
-  //define a QuizItem object to store the question and the correct answer
+  //Define a QuizItem object to store the question and the correct answer
+  var questionNumber = 0;
+
   var quizItem = {
     id: "",
     question: "",
-    answerOne: "",
-    answerTwo: "",
-    answerThree: "",
-    answerFour: "",
-    correctAnswer: "",
+    answerOne: {
+      answerText: "",
+      correct: false,
+    },
+    answerTwo: {
+      answerText: "",
+      correct: false,
+    },
+    answerThree: {
+      answerText: "",
+      correct: false,
+    },
+    answerFour: {
+      answerText: "",
+      correct: false,
+    },
   };
 
-  //define quiz
+  //Define quiz
   var quiz = {
     quizItem: [],
     score: 0,
     highscore: 0,
+
+    //Initialize function is used to load 10 questions into the quiz
+    initialize: function () {
+      //populate quiz wuth 1st question
+      quiz.quizItem.push({
+        id: "1",
+        question:
+          "Who did Patrick Roy Goalie for in 2001 and win the Stanley Cup",
+        answerOne: { answerText: "Los Angeles Kings", correct: false },
+        answerTwo: { answerText: "Colorado Avalanche", correct: true },
+        answerThree: { answerText: "Pittsburgh Penguins", correct: false },
+        answerFour: { answerText: "New York Rangers", correct: false },
+      });
+
+      // //populate quiz wuth 2nd question
+      quiz.quizItem.push({
+        id: "2",
+        question: "Which NFL Team won the 1st two Superbowls?",
+        answerOne: { answerText: "Chicago Bears", correct: false },
+        answerTwo: { answerText: "New York Jets", correct: false },
+        answerThree: { answerText: "Minnesota Vikings", correct: false },
+        answerFour: { answerText: "Greenbay Packers", correct: true },
+      });
+
+      // //populate quiz wuth 3rd question
+      quiz.quizItem.push({
+        id: "3",
+        question:
+          "What MLB Baseball team loast in the Worls Series to St Louis in 7 games in 1982?",
+        answerOne: { answerText: "New Your Yankees", correct: false },
+        answerTwo: { answerText: "Boston Red Sox", correct: false },
+        answerThree: { answerText: "Milwaukee Brewers", correct: true },
+        answerFour: { answerText: "Atlanta Braves", correct: false },
+      });
+
+      // //populate quiz wuth 4th question
+      quiz.quizItem.push({
+        id: "4",
+        question: "How many Super Bowl wins do the Greenbay Packers have?",
+        answerOne: { answerText: "1", correct: false },
+        answerTwo: { answerText: "2", correct: false },
+        answerThree: { answerText: "3", correct: false },
+        answerFour: { answerText: "4", correct: true },
+      });
+
+      // //populate quiz wuth 5th question
+      quiz.quizItem.push({
+        id: "5",
+        question: "What team did Joe Montana play most of his career with?",
+        answerOne: { answerText: "San Francisco 49ers", correct: true },
+        answerTwo: { answerText: "Chicago Bears", correct: false },
+        answerThree: { answerText: "Miami Dolphins", correct: false },
+        answerFour: { answerText: "New York Jets", correct: false },
+      });
+
+      // //populate quiz wuth 6th question
+      quiz.quizItem.push({
+        id: "6",
+        question: "Who wore the retired number 21 for the Colorado Avalanche?",
+        answerOne: { answerText: "Adam Foote", correct: false },
+        answerTwo: { answerText: "Patrick Roy", correct: false },
+        answerThree: { answerText: "Peter Forsberg", correct: true },
+        answerFour: { answerText: "Joe Sakic", correct: false },
+      });
+
+      //populate quiz wuth 8th question
+      quiz.quizItem.push({
+        id: "8",
+        question: "How many teams are there in Major League Baseball?",
+        answerOne: { answerText: "24", correct: false },
+        answerTwo: { answerText: "26", correct: false },
+        answerThree: { answerText: "28", correct: false },
+        answerFour: { answerText: "30", correct: true },
+      });
+
+      //populate quiz wuth 9th question
+      quiz.quizItem.push({
+        id: "9",
+        question: "Who is the worst Football Team in the NFL?",
+        answerOne: { answerText: "Chicago Bears", correct: true },
+        answerTwo: { answerText: "Dallas Cowboys", correct: false },
+        answerThree: { answerText: "Minnesota Vikings", correct: false },
+        answerFour: { answerText: "Oakland Raiders", correct: false },
+      });
+
+      //populate quiz wuth 10th question
+      quiz.quizItem.push({
+        id: "10",
+        question: "Which team won the World Series in 2019?",
+        answerOne: { answerText: "New York Yahkees", correct: false },
+        answerTwo: { answerText: "San Francisco Giants", correct: false },
+        answerThree: { answerText: "Arizona Diamondbacks", correct: false },
+        answerFour: { answerText: "Washington Nationals", correct: true },
+      });
+    },
   };
 
-  //funcitoon to load the quiz questions into the quiz
-  function loadQuizQuestions() {
-    quiz.quizItem.push({
-      id: "1",
-      question:
-        "Who did Patrick Roy Goalie for in 2001 and win the Stanley Cup",
-      answerOne: "Los Angeles Kings",
-      answerTwo: "Colorado Avalanche",
-      answerThree: "Pittsburgh Penguins",
-      answerFour: "New York Rangers",
-      correctAnswer: "answerTwo",
-    });
+  function displayQuestion(i) {
+    $("#quiz-question").text(quiz.quizItem[i].question);
+    // var answer1 = $("<button>");
+    // var answer2 = $("<button>");
+    // var answer3 = $("<button>");
+    // var answer4 = $("<button>");
 
-    //populate quiz wuth 2nd question
-    quiz.quizItem.push({
-      id: "2",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
+    // answer1.addClass("btn-block");
+    // answer1.addClass("btn-primary");
+    // answer1.attr("type", "button");
+    // answer1.attr("id", "answer-1");
+    $("#answer-1").text(quiz.quizItem[i].answerOne.answerText);
 
-    //populate quiz wuth 3rd question
-    quiz.quizItem.push({
-      id: "3",
-      question:
-        "What MLB Baseball team loast in the Worls Series to St Louis in 7 games in 1982?",
-      answerOne: "New Your Yankees",
-      answerTwo: "Boston Red Sox",
-      answerThree: "Milwaukee Brewers",
-      answerFour: "Atlanta Braves",
-      correctAnswer: "answerThree",
-    });
+    // answer2.addClass("btn-block");
+    // answer2.addClass("btn-primary");
+    // answer2.attr("type", "button");
+    // answer2.attr("id", "answer-2");
+    $("#answer-2").text(quiz.quizItem[i].answerTwo.answerText);
 
-    //populate quiz wuth 4th question
-    quiz.quizItem.push({
-      id: "4",
-      question: "How many Super Bowl wins do the Greenbay Packers have?",
-      answerOne: "1",
-      answerTwo: "2",
-      answerThree: "3",
-      answerFour: "4",
-      correctAnswer: "answerFour",
-    });
+    // answer3.addClass("btn-block");
+    // answer3.addClass("btn-primary");
+    // answer3.attr("type", "button");
+    // answer3.attr("id", "answer-3");
+    $("#answer-3").text(quiz.quizItem[i].answerThree.answerText);
 
-    //populate quiz wuth 5th question
-    quiz.quizItem.push({
-      id: "5",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
+    // answer4.addClass("btn-block");
+    // answer4.addClass("btn-primary");
+    // answer4.attr("type", "button");
+    // answer4.attr("id", "answer-4");
+    $("#answer-4").text(quiz.quizItem[i].answerFour.answerText);
 
-    //populate quiz wuth 6th question
-    quiz.quizItem.push({
-      id: "6",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
-
-    //populate quiz wuth 7th question
-    quiz.quizItem.push({
-      id: "7",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
-
-    //populate quiz wuth 8th question
-    quiz.quizItem.push({
-      id: "8",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
-
-    //populate quiz wuth 9th question
-    quiz.quizItem.push({
-      id: "9",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
-
-    //populate quiz wuth 10th question
-    quiz.quizItem.push({
-      id: "10",
-      question: "Which NFL Team won the 1st two Superbowls?",
-      answerOne: "Chicago Bears",
-      answerTwo: "New York Jets",
-      answerThree: "Minnesota Vikings",
-      answerFour: "Greenbay Packers",
-      correctAnswer: "answerFour",
-    });
+    // $("#answer-display").append(answer1);
+    // $("#answer-display").append(answer2);
+    // $("#answer-display").append(answer3);
+    // $("#answer-display").append(answer4);
+    // console.log("right here");
   }
 
-  //Start of executing the quiz
+  quiz.initialize();
 
-  loadQuizQuestions();
+  $("#btn-start").on("click", function () {
+    $("#quiz-intro").hide();
+    $("#btn-start").hide();
+    displayQuestion(questionNumber);
+  });
 
-  for (var i = 0; i < quiz.quizItem.length; i++) {
-    console.log(quiz.quizItem[i].id);
-    console.log(quiz.quizItem[i].question);
-    console.log(quiz.quizItem[i].answerOne);
-    console.log(quiz.quizItem[i].answerTwo);
-    console.log(quiz.quizItem[i].answerThree);
-    console.log(quiz.quizItem[i].answerFour);
-    console.log(quiz.quizItem[i].correctAnswer);
-  }
+  $("#answer-1").on("click", function () {
+    questionNumber++;
+    displayQuestion(questionNumber);
+  });
+
+  //   for (var i = 0; i < quiz.quizItem.length; i++) {
+  //     console.log(quiz.quizItem[i].id);
+  //     console.log(quiz.quizItem[i].question);
+  //     console.log(quiz.quizItem[i].answerOne.answerText);
+  //     console.log(quiz.quizItem[i].answerOne.correct);
+  //     console.log(quiz.quizItem[i].answerTwo.answerText);
+  //     console.log(quiz.quizItem[i].answerTwo.correct);
+  //     console.log(quiz.quizItem[i].answerThree.answerText);
+  //     console.log(quiz.quizItem[i].answerThree.correct);
+  //     console.log(quiz.quizItem[i].answerFour.answerText);
+  //     console.log(quiz.quizItem[i].answerFour.correct);
+  //   }
 });
